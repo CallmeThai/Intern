422f89601c9d9ae2bca61d16578b8f2c8f939131
CallmeThai <qthai2401@gmail.com>
2024-07-17 14:25:55 +0700

    update

---
+++ javascript.js
@@ -0,0 +1,10 @@
+console.log('motherfker')
+
+function ShowPopup() {
+    var popup = document.getElementById('popup');
+    popup.classList.add('show')
+}
+function hidePopup() {
+    var popup = document.getElementById('popup');
+    popup.classList.remove('show')
+}
